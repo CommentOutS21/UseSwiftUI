135bd1dd79286197588a49aa6981ede2011ffbbf
s2108tomoka <s2108.ochitomoka@gmail.com>
2024-12-14 17:28:57 +0900

    Initial Commit

---
+++ UseSwiftUI/UseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseSwiftUIApp.swift
+//  UseSwiftUI
+//
+//  Created by 越智友香 on 2024/12/14.
+//
+
+import SwiftUI
+
+@main
+struct UseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
